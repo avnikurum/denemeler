75d42cf11549c95e55e1ab199fca613836ccb7b7
avnikurum <47662302+avnikurum@users.noreply.github.com>
2021-04-28 12:56:15 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        System.out.println("A:" + a + " " + "B:" + b);
+
+        a = a + b; // a nın değerini 3 yapıyoruz
+        b = a - b; // (a+b)-b yaparak a'daki değeri b'ye atıyoruz
+        a = a - b; // a-(a-b) yaparak b'deki değeri a'ya atıyoruz
+        
+        System.out.println("replaced A:" + a + " " + "replaced B:" + b);
+
+
+    }
+}
